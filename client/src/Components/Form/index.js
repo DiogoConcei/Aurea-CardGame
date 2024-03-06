b6a0c3012319c4de20802a338fabf38f23aa93e1
Diogo <diogooliveiraspot@gmail.com>
2024-03-06 17:10:37 -0300

    formik-yup

--- client/src/Components/Form/index.js
+++ client/src/Components/Form/index.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from 'yup'
 import "./Form.css";
 
+const validationSchema = Yup.object().shape({
+  nomeBaralho: Yup.string().required('Campo obrigatório'),
+  description: Yup.string().max(150, 'Faça uma descrição de até 150 caracteres').required('Por favor, preencha o campo')
+})
+
 const CriaBaralho = () => {
+
   return (
     <section className="CreateDeck">
       <div className="BackDeck">
@@ -14,25 +21,52 @@ const CriaBaralho = () => {
           jogadas da vida.
         </p>
       </div>
-      <form className="FormPost">
-        <input type="text" id="nomeBaralho" placeholder="Nome do baralho" />
-        <label htmlFor="novaImagem" className="novaImagem">
-          Envie sua nova imagem
-        </label>
-        <input
-          type="file"
-          id="novaImagem"
-          className="input-file"
-          accept="image/*"
-        />
-        <textarea
-          id="descricaoBaralho"
-          placeholder="Descrição do baralho" spellCheck="false"
-        ></textarea>
-        <button className="FormSubmit" type="button" onclick="">
-          Criar Baralho
-        </button>
-      </form>
+      <Formik
+        initialValues={{
+          nomeBaralho: "Patos flamejantes",
+          descricaoBaralho:
+            "Em cada carta deste baralho, há um convite para explorar novos horizontes, desafiar limites e encontrar alegria nas mais inesperadas jogadas da vida.",
+          ImageInput: null
+        }}
+        validationSchema={validationSchema}
+        className="FormikPost"
+      >
+        <Form className="FormPost">
+          <Field
+            type="text"
+            id="nomeBaralho"
+            className="NomeBaralho"
+            name="nomeBaralho"
+            placeholder="Nome do baralho"
+          />
+          <ErrorMessage name="nomeBaralho" component="div" />
+
+          <label htmlFor="novaImagem" className="novaImagem">
+            Envie sua nova imagem
+          </label>
+          <Field
+            type="file"
+            id="novaImagem"
+            className="input-file"
+            name="ImageInput"
+            accept="image/*"
+          />
+          <ErrorMessage name="ImageInput" component="div" />
+
+          <Field
+                as="textarea"
+                id="description"
+                name="description"
+                className="descricaoBaralho"
+                placeholder="Enter your description"          />
+          <ErrorMessage name="description" component="div" />
+
+
+          <button className="FormSubmit" type="submit">
+            Criar Baralho
+          </button>
+        </Form>
+      </Formik>
     </section>
   );
 };
